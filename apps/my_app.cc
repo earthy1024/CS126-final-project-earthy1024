678e86c4fd5dfcd27ab8b418c58f00e1f84bc86a
earthy1024 <34816525+earthy1024@users.noreply.github.com>
2020-04-26 23:18:44 -0400

    Updated basic drawing functionality

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -3,30 +3,69 @@
 #include "my_app.h"
 
 #include <cinder/app/App.h>
-
-#include <../blocks/Cinder-QRCodeGenerator/src/QRCodeGenerator.h>
+#include <cinder/Font.h>
+#include <cinder/Text.h>
+#include <cinder/gl/draw.h>
+#include <cinder/gl/gl.h>
+#include <cinder/Vector.h>
 
 
 namespace myapp {
 
+using cinder::Color;
 using cinder::app::KeyEvent;
+using cinder::app::MouseEvent;
+using cinder::TextBox;
+
+bool begin_draw = true;
+bool draw_finished = false;
 
 MyApp::MyApp() { }
 
 void MyApp::setup() {
-
+    cinder::gl::enableDepthWrite();
+    cinder::gl::enableDepthRead();
 }
 
 void MyApp::update() {
-
-    QRCodeGenerator generator = QRCodeGenerator();
-    Surface8u image = generator.generate("test string", 5);
+    if (!draw_finished) {
+        return;
+    }
+    else {
+        MouseEvent event = MouseEvent();
+        mouseDown(event);
+    }
 }
 
 void MyApp::draw() {
+    cinder::gl::enableAlphaBlending();
+    if (begin_draw) {
+        cinder::gl::clear(Color(0, 0, 0));
+        begin_draw = false;
+    }
+    MouseEvent event = MouseEvent();
 
 }
 
-void MyApp::keyDown(KeyEvent event) { }
+void MyApp::drawPaint(MouseEvent event) const {
+    cinder::gl::color((float) rand() / (float) RAND_MAX
+            , (float) rand() / (float) RAND_MAX
+            , (float) rand() / (float) RAND_MAX);
+    cinder::gl::drawSolidCircle(event.getPos(), 20);
+
+}
+void MyApp::keyDown(KeyEvent event) {
+
+}
+
+
+void MyApp::mouseDown(MouseEvent event) {
+    if (event.isLeftDown()) {
+        drawPaint(event);
+    }
+
+
+}
+
 
 }  // namespace myapp
