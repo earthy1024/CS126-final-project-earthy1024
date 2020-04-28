f453fa5e3dbb2771f7f0a61b93a8af8eba465c94
earthy1024 <34816525+earthy1024@users.noreply.github.com>
2020-04-28 17:14:13 -0400

    Update my_app.h

--- apps/my_app.h
+++ apps/my_app.h
@@ -10,11 +10,32 @@ namespace myapp {
 
 class MyApp : public cinder::app::App {
  public:
+    enum class PaintColor {
+        Red,
+        Blue,
+        Green,
+        Orange,
+        Yellow
+    };
+    enum class PaintSize {
+        One = 5,
+        Two = 10,
+        Three = 15,
+        Four = 20
+    };
+
+
   MyApp();
   void setup() override;
   void update() override;
   void draw() override;
+  static void SelectColor(const std::string& text);
+  static void SelectSize(const std::string& text);
+  void DrawPaint(cinder::app::MouseEvent) const;
   void keyDown(cinder::app::KeyEvent) override;
+  void mouseDown(cinder::app::MouseEvent) override;
+  void mouseDrag(cinder::app::MouseEvent) override;
+  static void CreateButton(const std::string& text, const cinder::ivec2& size, const cinder::vec2& loc);
 };
 
 }  // namespace myapp
