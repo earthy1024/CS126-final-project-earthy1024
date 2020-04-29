19f28690b5a5d000778ec6ea8ead7fabc7d4cd67
earthy1024 <34816525+earthy1024@users.noreply.github.com>
2020-04-28 21:20:51 -0400

    Create converter.h

---
+++ include/mylibrary/converter.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 [Your Name]. All rights reserved.
+#ifndef FINALPROJECT_MYLIBRARY_CONVERTER_H_
+#define FINALPROJECT_MYLIBRARY_CONVERTER_H_
+
+#include <cinder/ImageIo.h>
+#include <cinder/Surface.h>
+
+namespace mylibrary {
+
+void ConvertImage(cinder::Surface surface, std::string name);
+
+}  // namespace mylibrary
+
+
+#endif // FINALPROJECT_MYLIBRARY_CONVERTER_H_
