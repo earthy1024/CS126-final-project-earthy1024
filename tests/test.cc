769ff5aeda0c75a7823deb3a125a04257c7b1472
earthy1024 <34816525+earthy1024@users.noreply.github.com>
2020-05-06 23:32:25 -0400

    Update test.cc

--- tests/test.cc
+++ tests/test.cc
@@ -5,11 +5,27 @@
 #include <catch2/catch.hpp>
 #include <cinder/Rand.h>
 
-#include <mylibrary/example.h>
-
+#include <mylibrary/converter.h>
+#include "C:\Users\HP\CLionProjects\Cinder\MyProjects\final-project-earthy1024\apps\my_app.h"
 
 TEST_CASE("Random sanity test", "[random]") {
   const float random = cinder::randFloat();
   REQUIRE(0. <= random);
   REQUIRE(random <= 1.);
+}
+
+TEST_CASE("Correct image url") {
+    std::string url = "https://imgur.com/";
+    REQUIRE(mylibrary::ConnectToHost("C:/Users/HP/Documents/CinderFiles/TestUpload.jpg") != url);
+}
+
+TEST_CASE("Get correct qr code image") {
+    std::string url = "www.imgur.com";
+    std::string path = "C:/Users/HP/Documents/CinderFiles/www.imgur.com/.jpg";
+    REQUIRE(mylibrary::CreateQRCode(url) == path);
+}
+
+TEST_CASE("Get correct qr code from web link") {
+    std::string url = mylibrary::ConnectToHost("C:/Users/HP/Documents/CinderFiles/TestUpload.jpg");
+    REQUIRE(mylibrary::CreateQRCode(url) == "C:/Users/HP/Documents/CinderFiles/https://imgur.com/a/yisyZik/.jpg");
 }
