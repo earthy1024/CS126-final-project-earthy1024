60a193832e609e8ef1df7ec0c4084c93f57b4659
earthy1024 <34816525+earthy1024@users.noreply.github.com>
2020-05-06 20:52:02 -0400

    Allow for user input of image file

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,5 +1,3 @@
-// Copyright (c) 2020 [Your Name]. All rights reserved.
-
 #include "my_app.h"
 #include <mylibrary/converter.h>
 
@@ -11,154 +9,222 @@
 #include <cinder/Vector.h>
 #include <cinder/params/Params.h>
 #include <cinder/app/RendererGl.h>
-#include <rpc.h>
-#include <list>
+#include <direct.h>
+#include <windows.h>
+#include <windows.ui.xaml.resources.h>
+#include <Commdlg.h>
+#include <tchar.h>
+
 
 namespace myapp {
 
-using cinder::Color;
-using cinder::app::KeyEvent;
-using cinder::app::MouseEvent;
-using cinder::TextBox;
+    using cinder::Color;
+    using cinder::app::KeyEvent;
+    using cinder::app::MouseEvent;
+    using cinder::TextBox;
+    cinder::params::InterfaceGlRef mParams;
+    bool begin_draw = true;
+    bool draw_finished = false;
+    bool start_type = false;
+    bool path_typed = false;
+    bool capital = false;
+    std::string filename = "C:/Users/HP/Documents/CinderFiles/";
+    std::vector<cinder::vec2> button_list;
+    std::vector<std::string> button_name;
+    MyApp::PaintSize paint_size = MyApp::PaintSize::One;
+    MyApp::PaintColor paint_color = MyApp::PaintColor::Red;
+    cinder::ivec2 button_size = cinder::ivec2(60, 30);
+    cinder::Surface current;
+    OPENFILENAME ofn;
+    char szFile[260];
+    HWND hwnd;
+    HANDLE hf;
 
-cinder::params::InterfaceGlRef mParams;
-bool begin_draw = true;
-bool draw_finished = false;
-std::vector<cinder::vec2> button_list;
-std::vector<std::string> button_name;
-MyApp::PaintSize paint_size = MyApp::PaintSize::One;
-MyApp::PaintColor paint_color = MyApp::PaintColor::Red;
-cinder::ivec2 button_size = cinder::ivec2(50, 30);
+    MyApp::MyApp() { }
 
-MyApp::MyApp() { }
+    void MyApp::setup() {
+        cinder::gl::enableDepthWrite();
+        cinder::gl::enableDepthRead();
 
-void MyApp::setup() {
-    cinder::gl::enableDepthWrite();
-    cinder::gl::enableDepthRead();
-
-}
-
-void MyApp::update() {
-    if (draw_finished) {
-        return;
     }
-    else {
-        if (paint_color == PaintColor::Red) {
 
+    void MyApp::update() {
+        if (draw_finished) {
+            return;
+        } else if (start_type) {
+            cinder::gl::clear(Color(0, 0, 0));
+            UpdateFileName();
+            DrawButtons();
+        } else if (path_typed) {
+            cinder::gl::clear(Color(0, 0, 0));
+            cinder::Surface image = cinder::Surface(cinder::loadImage(filename));
+            cinder::gl::Texture2dRef texture2 = cinder::gl::Texture::create(image);
+            cinder::gl::draw(texture2);
+            cinder::gl::Texture2dRef texture1 = cinder::gl::Texture::create(current);
+            cinder::gl::draw(texture1);
+
+            DrawButtons();
+            path_typed = false;
+        }
+
+
+    }
+    cinder::Surface MyApp::copyDraw() {
+        cinder::Surface surface = copyWindowSurface(cinder::Area(0, 0, 700, 700));
+        return surface;
+    }
+
+    void MyApp::draw() {
+        cinder::gl::enableAlphaBlending();
+        if (begin_draw || draw_finished) {
+            cinder::gl::clear(Color(0, 0, 0));
+            begin_draw = false;
+            draw_finished = false;
+            // button("");
+            DrawButtons();
         }
     }
-}
-
-void MyApp::draw() {
-    cinder::gl::enableAlphaBlending();
-    if (begin_draw) {
-        cinder::gl::clear(Color(0, 0, 0));
-        begin_draw = false;
-        // button("");
-        CreateButton("Blue", button_size, cinder::vec2(750, 100));
-        CreateButton("Green", button_size, cinder::vec2(750, 150));
-        CreateButton("Red", button_size, cinder::vec2(750, 200));
-        CreateButton("Small", button_size, cinder::vec2(750, 270));
-        CreateButton("Medium", button_size, cinder::vec2(750, 320));
-        CreateButton("Big", button_size, cinder::vec2(750, 370));
-        CreateButton("Large", button_size, cinder::vec2(750, 420));
-        CreateButton("Submit", button_size, cinder::vec2(500, 800));
+    void MyApp::DrawButtons() {
+        CreateButton("Blue", button_size, cinder::vec2(750, 70), cinder::Color(0, 0, 1));
+        CreateButton("Green", button_size, cinder::vec2(750, 120), cinder::Color(0, 1, 0));
+        CreateButton("Red", button_size, cinder::vec2(750, 170), cinder::Color(1, 0, 0));
+        CreateButton("Orange", button_size, cinder::vec2(750, 220), cinder::Color(1, .8, 0));
+        CreateButton("Teal", button_size, cinder::vec2(750, 270), cinder::Color(0, 1, 1));
+        CreateButton("Purple", button_size, cinder::vec2(750, 320), cinder::Color(1, 0, 1));
+        CreateButton("Small", button_size, cinder::vec2(750, 370), cinder::Color(0, 0, 0));
+        CreateButton("Medium", button_size, cinder::vec2(750, 420), cinder::Color(0, 0, 0));
+        CreateButton("Big", button_size, cinder::vec2(750, 470), cinder::Color(0, 0, 0));
+        CreateButton("Large", button_size, cinder::vec2(750, 520), cinder::Color(0, 0, 0));
+        CreateButton("Image", button_size, cinder::vec2(750, 570), cinder::Color(0, 0, 0));
+        CreateButton("Submit", button_size, cinder::vec2(750, 620), cinder::Color(0, 0, 0));
+        cinder::gl::color(1, 1, 1);
+        cinder::gl::drawSolidRect(cinder::Rectf(700, 50, 800, 700));
     }
 
-}
-
-void MyApp::DrawPaint(MouseEvent event) const {
-    if (paint_color == PaintColor::Red) {
-        cinder::gl::color(1, 0, 0);
-    } else if (paint_color == PaintColor::Green) {
-        cinder::gl::color(0, 1, 0);
-    } else if (paint_color == PaintColor::Blue) {
-        cinder::gl::color(0, 0, 1);
+    void MyApp::DrawPaint(const MouseEvent& event) const {
+        if (paint_color == PaintColor::Red) {
+            cinder::gl::color(1, 0, 0);
+        } else if (paint_color == PaintColor::Green) {
+            cinder::gl::color(0, 1, 0);
+        } else if (paint_color == PaintColor::Blue) {
+            cinder::gl::color(0, 0, 1);
+        } else if (paint_color == PaintColor::Orange) {
+            cinder::gl::color(1, .8, 0);
+        } else if (paint_color == PaintColor::Teal) {
+            cinder::gl::color(0, 1, 1);
+        } else if (paint_color == PaintColor::Purple) {
+            cinder::gl::color(1, 0, 1);
+        }
+        cinder::gl::drawSolidCircle(event.getPos(), static_cast<float>(paint_size));
     }
-    cinder::gl::drawSolidCircle(event.getPos(), static_cast<float>(paint_size));
-}
 
-void MyApp::keyDown(KeyEvent event) {
+    void MyApp::keyDown(KeyEvent event) {
+        if (event.getCode() == KeyEvent::KEY_RSHIFT || event.getCode() == KeyEvent::KEY_LSHIFT) {
+           capital = true;
+           return;
+        }
+        if (event.getChar() == ' ') {
+            path_typed = true;
+            start_type = false;
+            return;
+        }
+        if (capital) {
+            filename += toupper(event.getChar());
+            capital = false;
+            return;
+        }
+        filename += event.getChar();
+    }
 
-}
 
-
-void MyApp::mouseDown(MouseEvent event) {
-    if (event.isLeftDown()) {
-        for (int index = 0; index < button_list.size(); index++) {
-            if (event.getPos().x < button_list[index].x + 25
-            &&  event.getPos().x > button_list[index].x - 25
-            &&  event.getPos().y < button_list[index].y + 15
-            &&  event.getPos().y > button_list[index].y - 15) {
-                if (button_name[index] == "Submit") {
-                    cinder::Surface surface = copyWindowSurface();
-                    // Add user input code
-                    mylibrary::ConvertImage(surface, "TestTwo");
-
-                    // This segment can be used to open QRCode surface
-                    cinder::gl::clear(Color(0, 0, 0));
-                    cinder::gl::color(1, 1, 1);
-                    cinder::Surface newSurface = cinder::Surface(cinder::loadImage("C:/Users/HP/Documents/CinderFiles/testqrcode.png"));
-                    cinder::gl::Texture2dRef texture = cinder::gl::Texture::create(newSurface);
-                    cinder::gl::draw(texture);
+    void MyApp::mouseDown(MouseEvent event) {
+        if (event.isLeftDown()) {
+            for (int index = 0; index < button_list.size(); index++) {
+                if (event.getPos().x < button_list[index].x + 30
+                    &&  event.getPos().x > button_list[index].x - 30
+                    &&  event.getPos().y < button_list[index].y + 15
+                    &&  event.getPos().y > button_list[index].y - 15) {
+                    if (button_name[index] == "Image") {
+                        start_type = true;
+                        path_typed = false;
+                        current = copyDraw();
+                        UpdateFileName();
+                    } else if (button_name[index] == "Submit") {
+                        mylibrary::ConvertImage(copyDraw(), "CropTest");
+                    }
+                    SelectColor(button_name[index]);
+                    SelectSize(button_name[index]);
                 }
-                SelectColor(button_name[index]);
-                SelectSize(button_name[index]);
             }
         }
     }
-}
 
-void MyApp::mouseDrag(MouseEvent event) {
-    if (event.isLeftDown()) {
-        DrawPaint(event);
+    void MyApp::UpdateFileName() {
+        CreateButton("File: " + filename, cinder::ivec2(400, 30), cinder::vec2(400, 400), cinder::Color(0, 0, 0));
     }
-}
 
-
-void MyApp::SelectColor(const std::string& text) {
-    if (text == "Red") {
-        paint_color = PaintColor::Red;
-    } else if (text == "Green") {
-        paint_color = PaintColor::Green;
-    } else if (text == "Blue") {
-        paint_color = PaintColor::Blue;
+    void MyApp::mouseDrag(MouseEvent event) {
+        if (event.isLeftDown()) {
+            DrawPaint(event);
+        }
     }
-}
 
-void MyApp::SelectSize(const std::string& text) {
-    if (text == "Small") {
-        paint_size = PaintSize::One;
-    } else if (text == "Medium") {
-        paint_size = PaintSize::Two;
-    } else if (text == "Big") {
-        paint_size = PaintSize::Three;
-    } else if (text == "Large") {
-        paint_size = PaintSize::Four;
+
+    void MyApp::SelectColor(const std::string& text) {
+        if (text == "Red") {
+            paint_color = PaintColor::Red;
+        } else if (text == "Green") {
+            paint_color = PaintColor::Green;
+        } else if (text == "Blue") {
+            paint_color = PaintColor::Blue;
+        } else if (text == "Orange") {
+            paint_color = PaintColor::Orange;
+        } else if (text == "Teal") {
+            paint_color = PaintColor::Teal;
+        } else if (text == "Purple") {
+            paint_color = PaintColor::Purple;
+        }
     }
-}
+
+    void MyApp::SelectSize(const std::string& text) {
+        if (text == "Small") {
+            paint_size = PaintSize::One;
+        } else if (text == "Medium") {
+            paint_size = PaintSize::Two;
+        } else if (text == "Big") {
+            paint_size = PaintSize::Three;
+        } else if (text == "Large") {
+            paint_size = PaintSize::Four;
+        }
+    }
 
 
-void MyApp::CreateButton(const std::string& text, const cinder::ivec2& size, const cinder::vec2& loc) {
-    cinder::gl::color(Color::white());
+    void MyApp::CreateButton(std::string text, const cinder::ivec2& size, const cinder::vec2& loc, cinder::Color color) {
+        button_list.push_back(loc);
+        button_name.push_back(text);
 
-    button_list.push_back(loc);
-    button_name.push_back(text);
+        if (color != cinder::Color(0, 0, 0)) {
+            text = "";
+        }
+        cinder::gl::color(Color::white());
 
-    auto box = TextBox()
-        .alignment(TextBox::CENTER)
-        .font(cinder::Font("Arial", 20))
-        .size(size)
-        .color(Color::black())
-        .backgroundColor(cinder::ColorA(1, 1, 1))
-        .text(text);
 
-    const auto box_size = box.getSize();
-    const cinder::vec2 locp = {loc.x - box_size.x / 2, loc.y - box_size.y / 2};
-    const auto surface = box.render();
-    const auto texture = cinder::gl::Texture::create(surface);
-    cinder::gl::draw(texture, locp);
-}
+
+        auto box = TextBox()
+                .alignment(TextBox::CENTER)
+                .font(cinder::Font("Arial", 20))
+                .size(size)
+                .color(Color::white())
+                .backgroundColor(cinder::ColorA(color))
+                .text(text);
+
+        const auto box_size = box.getSize();
+        const cinder::vec2 locp = {loc.x - box_size.x / 2, loc.y - box_size.y / 2};
+        const auto surface = box.render();
+        const auto texture = cinder::gl::Texture::create(surface);
+        cinder::gl::draw(texture, locp);
+
+    }
 
 
 }  // namespace myapp
